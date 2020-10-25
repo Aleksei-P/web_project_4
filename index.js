9edc25456e78cabfccf3667da703cf1b37f8a022
Aleksei Podovinnikov <alex-xoxa@yandex.ru>
2020-10-25 16:29:37 -0700

    popup_box JS

---
+++ index.js
@@ -0,0 +1,24 @@
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.modal__button');
+const form = document.querySelector('.form');
+const modal = document.querySelector('.modal');
+const nameInput = document.querySelector('.form__input_type_name');
+const infoInput = document.querySelector('.form__input_type_info');
+const profileName = document.querySelector('.popup__value_type_name');
+const profileInfo = document.querySelector('.popup__value_type_info');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileInfo.textContent = infoInput.value;
+
+  toogleModal()
+
+})
+
+function toogleModal() {
+   modal.classList.toggle('modal_open');
+}
+
+editButton.addEventListener('click', toogleModal)
+closeButton.addEventListener('click', toogleModal)
