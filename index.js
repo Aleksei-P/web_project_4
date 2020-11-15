b3da2b4c6f85ab17bc59e3df09cae5292c494b5c
Aleksei Podovinnikov <alex-xoxa@yandex.ru>
2020-11-14 18:26:11 -0800

    js modal card, add form, delete, like

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.modal__button');
-const form = document.querySelector('.form');
+//Sorry, I have been very confused with the code
+
+//modal windows
 const modal = document.querySelector('.modal');
+const modalEdit = document.querySelector('.modal_edit');
+const modalAdd = document.querySelector('.modal_add');
+const modalImage = document.querySelector('.modal_image');
+
+//
+const popupImage = modalImage.querySelector('.modal__popup-image');
+const popupImageTitle = modalImage.querySelector('.modal__popup-title');
+
+//button
+const addButton = document.querySelector('.profile__add-button');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = modal.querySelector('.modal__button');
+const closeAddButton = modalAdd.querySelector('.modal__button');
+const closePopupImage = modalImage.querySelector('.modal__button');
+const saveImage = modalAdd.querySelector('.form__button');
+
+
+//add new image
+const formAdd = modalAdd.querySelector('.form');
+const imageNewTitle = modalAdd.querySelector('.form__input_type_card-title');
+const imageNewLink = modalAdd.querySelector('.form__input_type_url');
+
+
+
+const form = document.querySelector('.form');
+
+
 const nameInput = document.querySelector('.form__input_type_name');
 const infoInput = document.querySelector('.form__input_type_info');
 const profileName = document.querySelector('.profile__title');
 const profileInfo = document.querySelector('.profile__subtitle');
 
+
+//profile form submit
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -14,12 +43,104 @@ form.addEventListener('submit', (event) => {
   profileInfo.textContent = infoInput.value;
 
   toggleModal()
-
 })
 
 function toggleModal() {
   modal.classList.toggle('modal_open');
-}
+};
 
-editButton.addEventListener('click', toggleModal)
-closeButton.addEventListener('click', toggleModal)
+function toggleModalAdd() {
+  modalAdd.classList.toggle('modal_open');
+};
+
+function toggleModalPopupImage() {
+  modalImage.classList.toggle('modal_open');
+};
+
+editButton.addEventListener('click', toggleModal);
+
+closeButton.addEventListener('click', toggleModal);
+
+closeAddButton.addEventListener('click', toggleModalAdd);
+
+closePopupImage.addEventListener('click', toggleModalPopupImage);
+
+
+addButton.addEventListener('click', () => {
+  toggleModalAdd();
+});
+
+//Cards <template>
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  },
+
+];
+
+
+initialCards.forEach(data => {
+  const cardTemplate = document.querySelector('.elements').content.querySelector('.element');
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.element__image');
+  const cardTitle = cardElement.querySelector('.element__title');
+  const cardLikeButton = cardElement.querySelector('.element__button');
+  const cardLikeButtonOn = cardElement.querySelector('.element__button_like')
+  const cardDelete = cardElement.querySelector('.element__delete');
+
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  const list = document.querySelector('.elements');
+  list.prepend(cardElement);
+
+  cardDelete.addEventListener('click', (e) => {
+    e.target.closest('.element').remove();
+  });
+
+  cardLikeButton.addEventListener('click', (e) => {
+    e.target.classList.toggle('element__button_like');
+  });
+
+  //open popup image
+
+  cardImage.addEventListener('click', () => {
+    toggleModalPopupImage();
+    popupImage.src = data.link;
+    popupImageTitle.textContent = data.name;
+
+})
+
+//new image submit
+
+  formAdd.addEventListener('submit', (event) => {
+    event.preventDefault();
+    cardTitle.textContent = imageNewTitle.value;
+    cardImage.src = imageNewLink.value;
+    initialCards.push(imageNewTitle.value, imageNewLink.value);
+
+    toggleModalAdd()
+  });
+
+})
